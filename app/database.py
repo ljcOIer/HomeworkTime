8356e4dccd93ab16d87bdbd09ad2471937ac77c6
ljcOIer <100518151+ljcOIer@users.noreply.github.com>
2026-03-08 17:51:36 +0800

    ？

--- app/database.py
+++ app/database.py
@@ -1,3 +1,23 @@
 import data_tool as o
 
 user = o.s("user")
+SchoolListDATA = o.s("SchoolList")
+
+
+def verify_cookies(cookies):
+    username = cookies.get('username')
+    pass_str = cookies.get('pass_str')
+    identity = cookies.get('identity')
+    school = cookies.get('school')
+    # return user.value
+    if not username or not pass_str or not identity or not school:
+        return False
+    if school not in user.value:
+        return False
+    if identity not in user.value[school]:
+        return False
+    if username not in user.value[school][identity]:
+        return False
+    if o.encrypt(user.value[school][identity][username]['password']) != pass_str:
+        return False
+    return True
--- data_tool.py
+++ data_tool.py
@@ -2,9 +2,18 @@ import json
 from io import *
 import random
 import string
+import hashlib
+
 def randstr(n): # 生成长度为 n 的随机字符串，为登录验证做准备
     all_characters = string.ascii_letters + string.digits
     return ''.join(random.choice(all_characters) for _ in range(n))
+
+def encrypt(content: str) -> str:
+    """
+    简化版MD5加密函数 仅支持字符串输入 返回32位16进制字符串
+    """
+    # 将字符串转为UTF-8编码的字节串，计算MD5并返回16进制结果
+    return hashlib.md5(content.encode('utf-8')).hexdigest()
 
 class s: # 数据库函数
     def __init__(self,name):
